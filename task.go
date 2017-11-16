19185fa9f830c26bba597ad1c19388ef9ea8cfe7
kcmerrill <kc.merrill@ibotta.com>
2017-11-16 16:05:00 -0700

    Added sprig templating funcs

--- task.go
+++ task.go
@@ -3,13 +3,14 @@ package main
 import (
 	"bytes"
 	"errors"
-	"html/template"
 	"io/ioutil"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"strings"
+	"text/template"
 
+	"github.com/Masterminds/sprig"
 	yaml "gopkg.in/yaml.v2"
 )
 
@@ -51,7 +52,8 @@ func (t *task) run() bool {
 	fns := template.FuncMap{
 		"task": taskParams,
 	}
-	tmpl, parseErr := template.New("params").Funcs(fns).Parse(t.CMD)
+
+	tmpl, parseErr := template.New("params").Funcs(sprig.TxtFuncMap()).Funcs(fns).Parse(t.CMD)
 	if parseErr != nil {
 		log("failed[template]", t.Q+":"+t.Name, false)
 		return false
